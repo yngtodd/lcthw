ffdd65765b1ffe890c34851e7eb46ffd10638f29
Todd <myoung44@vols.utk.edu>
2017-11-12 10:33:50 -0500

    Updating looping ex9

--- ex9/ex9.c
+++ ex9/ex9.c
@@ -1,11 +1,24 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]){
-    /* Simple While-loop. */
+    // create an array from 0 to 24
     int i = 0;
     while(i < 25){
         printf("%d ", i);
         i++;
+    }
+    printf("\n");
+
+    // create an array from 24 to 0.
+    int elem = 24;
+    int numbers[25] = {};
+    for(int i=0; i < 24; i++){
+        numbers[i] = elem;
+        elem--;
+    }
+
+    for(int i=0; i < 25; i++){
+        printf("%d ", numbers[i]);
     }
     printf("\n");
 
